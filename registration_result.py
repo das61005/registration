5dde2f41c39c01cd84a829a65b74ed098f47bbf4
Chen chun wei <112364393+das61005@users.noreply.github.com>
2022-09-02 15:40:55 +0800

    Update registration_result.py

--- registration_result.py
+++ registration_result.py
@@ -8,7 +8,7 @@ while(1):
     input_index=input("nodule index = ").split(",")  
     if input_index[0]=='c':
         break
-    output_index=(np.dot([int(input_index[0]),int(input_index[1]),int(input_index[2])],tranform_matrix_rot)+tranform_matrix_t)*scale
+    output_index=(np.dot([int(input_index[0]),int(input_index[1]),int(input_index[2])],tranform_matrix_rot.T)+tranform_matrix_t)*scale
     output_index=[ round(i) for i in output_index]
     print(output_index)
 
